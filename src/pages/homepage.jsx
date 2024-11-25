e5a36214da8b7419bf7f6a463677b47394aa5b4f
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-11-25 19:21:46 +0000

    NavBar añadido

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import NavBar from "src/components/navbar.jsx";
+import Content from "src/components/content.jsx";
+import Footer from "src/components/footer.jsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
